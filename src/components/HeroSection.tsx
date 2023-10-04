7746f64a428f6a9316a6e7a46540dbf137fbe11f
rulhuda <nurulhuda.unhasy@gmail.com>
2023-10-04 18:07:13 +0700

    Updated resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,65 +1,75 @@
-'use client'
-import Image from "next/image"
-import { Link } from "react-scroll/modules"
-import {HiArrowDown} from "react-icons/hi"
+"use client";
+import Image from "next/image";
+import NextLink from "next/link";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown, HiDownload } from "react-icons/hi";
 
 function HeroSection() {
   return (
-    <section id="home">
-      <div className="flex flex-col text-center items-center justify-center sm:py-32 my-12 py-16 md:flex-row md:space-x-4 md:text-left md:py-42">
-        <div className="md:w-1/2 md:mt-2 px-8">
-          <Image className={"rounded-full shadow-xl"} priority={true} src={"/Nurul-Huda.png"} alt="" width={300} height={300} />
+    <section id='home'>
+      <div className='flex flex-col text-center items-center justify-center sm:py-32 my-12 py-16 md:flex-row md:space-x-4 md:text-left md:py-42'>
+        <div className='md:w-1/2 md:mt-2 px-8'>
+          <Image
+            className={"rounded-full shadow-xl"}
+            priority={true}
+            src={"/Nurul-Huda.png"}
+            alt=''
+            width={300}
+            height={300}
+          />
         </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="font-bold text-4xl mt-6 md:text-7xl md:mt-0">Hi, I{"'"}m Huda!</h1>
-          <p className="text-2xl mt-4 mb-6">Im a {" "}
-          <span className="font-semibold text-teal-500">
-            Web Developer {" "}
-          </span>
-          based in Jombang, East Java.
+        <div className='md:mt-2 md:w-3/5'>
+          <h1 className='font-bold text-4xl mt-6 md:text-7xl md:mt-0'>
+            Hi, I{"'"}m Huda!
+          </h1>
+          <p className='text-2xl mt-4 mb-6'>
+            Im a{" "}
+            <span className='font-semibold text-teal-500'>Web Developer </span>
+            based in Jombang, East Java.
           </p>
-          <div className="flex flex-row space-x-4 justify-center md:justify-start">
-            <Link
-            to="projects"
-            className="text-neutral-100 cursor-pointer font-semibold px-6 py-3 bg-teal-500 rounded-md text-2xl"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
+          <div className='flex flex-row space-x-4 justify-center md:justify-start'>
+            <NextLink
+              rel='noopener noreferrer'
+              target='_blank'
+              download={true}
+              title='Download Portfolio'
+              href='/resume/Nurul-Huda_Portfolio.pdf'
+              className='text-neutral-100 cursor-pointer font-semibold px-6 py-3 bg-teal-500 rounded-md text-2xl'
             >
-              Projects
-            </Link>
+              <div className='flex flex-row'>
+                <HiDownload size={28} /> Portfolio
+              </div>
+            </NextLink>
             <Link
-            to="contact"
-            className="text-neutral-100 cursor-pointer font-semibold px-6 py-3 bg-indigo-500 rounded-md text-2xl"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
+              to='contact'
+              className='text-neutral-100 cursor-pointer font-semibold px-6 py-3 bg-indigo-500 rounded-md text-2xl'
+              activeClass='active'
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
             >
               Contact
             </Link>
           </div>
         </div>
       </div>
-        
-      <div className="flex flex-row justify-center">
+
+      <div className='flex flex-row justify-center'>
         <Link
-        className="cursor-pointer"
-        to="about"
-        activeClass="active"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}
+          className='cursor-pointer'
+          to='about'
+          activeClass='active'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
         >
-          <HiArrowDown size={30} className="animate-bounce" />
+          <HiArrowDown size={30} className='animate-bounce' />
         </Link>
       </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
