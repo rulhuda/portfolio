7bcb98e5a0e68d0a4a5731ce5f482aac1c7b3d19
Nurul Huda <nurulhuda.unhasy@gmail.com>
2025-02-12 20:51:35 +0700

    change text size & img

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -32,7 +32,7 @@ function HeroSection() {
             <Image
               className={"rounded-full shadow-xl"}
               priority={true}
-              src={"/me.jpg"}
+              src={"/Nurul-Huda.png"}
               alt=''
               width={300}
               height={300}
@@ -41,7 +41,7 @@ function HeroSection() {
           <div className='rounded-full absolute -inset-1 blur-md bg-gradient-to-br from-pink-500 via-cyan-500 to-violet-500 z-10'></div>
         </div>
         <div className='md:mt-2 md:w-3/5'>
-          <h1 className='text-4xl md:text-9xl mt-2 md:mt-0 font-extrabold bg-clip-text text-transparent drop-shadow-[1.6px_0.8px_rgba(0,0,0,0.8)] bg-[linear-gradient(to_right,theme(colors.green.300),theme(colors.green.100),theme(colors.sky.400),theme(colors.yellow.200),theme(colors.sky.400),theme(colors.green.100),theme(colors.green.300))] bg-[length:200%_auto] animate-gradient'>
+          <h1 className='text-4xl md:text-8xl mt-2 md:mt-0 font-extrabold bg-clip-text text-transparent drop-shadow-[1.6px_0.8px_rgba(0,0,0,0.8)] bg-[linear-gradient(to_right,theme(colors.green.300),theme(colors.green.100),theme(colors.sky.400),theme(colors.yellow.200),theme(colors.sky.400),theme(colors.green.100),theme(colors.green.300))] bg-[length:200%_auto] animate-gradient'>
             Hi, I{"'"}m Huda!
           </h1>
           <p className='text-2xl mt-4 mb-6 text-left'>
